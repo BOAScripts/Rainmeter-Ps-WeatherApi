611c7eef5af45852e329aa647d4197313ea33975
Alexis Botteldoorn <bottel.alexis@gmail.com>
2023-04-25 14:16:24 +0200

    Weather image process updated + skin Metadata updated

--- Ps-WeatherApi/@Resources/scripts/ApiWeather.ps1
+++ Ps-WeatherApi/@Resources/scripts/ApiWeather.ps1
@@ -1,49 +1,3 @@
-<#
-https://www.weatherapi.com/docs/
-
-    allData = {
-        "location": {
-            "name": "Court-Saint-Etienne",
-            "region": "",
-            "country": "Belgium",
-            "lat": 50.65,
-            "lon": 4.56,
-            "tz_id": "Europe/Brussels",
-            "localtime_epoch": 1682077935,
-            "localtime": "2023-04-21 13:52"
-        },
-        "current": {
-            "last_updated_epoch": 1682077500,
-            "last_updated": "2023-04-21 13:45",
-            "temp_c": 13.0,
-            "temp_f": 55.4,
-            "is_day": 1,
-            "condition": {
-                "text": "Partly cloudy",
-                "icon": "//cdn.weatherapi.com/weather/64x64/day/116.png",
-                "code": 1003
-            },
-            "wind_mph": 10.5,
-            "wind_kph": 16.9,
-            "wind_degree": 50,
-            "wind_dir": "NE",
-            "pressure_mb": 1011.0,
-            "pressure_in": 29.85,
-            "precip_mm": 0.0,
-            "precip_in": 0.0,
-            "humidity": 58,
-            "cloud": 50,
-            "feelslike_c": 12.3,
-            "feelslike_f": 54.2,
-            "vis_km": 10.0,
-            "vis_miles": 6.0,
-            "uv": 4.0,
-            "gust_mph": 8.1,
-            "gust_kph": 13.0
-        }
-    }
-#>
-
 # VARS #
 # !!! MODIFY THESE STING VALUES TO MATCH YOUR API KEY AND LOCATION #
 $apiKey = "YOUR-API-KEY-HERE"
@@ -82,14 +36,28 @@ try {
             LastUpdated = "Updated on: " + [string]$allData.current.last_updated
         }
         $myData | ConvertTo-Json | Out-File "./@Resources/scripts/response.json"
-        $imgName = "./@Resources/scripts/PNG/" + $myData.CondTxt + ".png"
+        
+        $imgName = "./@Resources/scripts/PNG/WeatherImg.png"
+        #$firstDL = $true        
         if (!(Test-Path $imgName)){
-            write-host "[+] Downloading condition img"
+            write-host "[+] First img download"
             get-conditionImg -imgName $imgName -imgUrl $myData.CondIcoUrl
         }
-        else {
-            Write-Host "[i] Condition img already downloaded"
+        else{
+            $localImgMd5 = (Get-FileHash $imgName -Algorithm MD5).Hash
+            $urlImgContent = (New-Object System.Net.WebClient).DownloadData($myData.CondIcoUrl)
+            $urlImgMd5Bytes = [System.Security.Cryptography.MD5]::Create().ComputeHash($urlImgContent)
+            $urlImgMd5 = [System.BitConverter]::ToString($urlImgMd5Bytes).Replace("-","").ToUpper()
+            if ($localImgMd5 -ne $urlImgMd5){
+                Write-Host "[+] Update WeatherImg.png"
+                get-conditionImg -imgName $imgName -imgUrl $myData.CondIcoUrl
+            }
+            else{
+                Write-Host "[i] MD5sum of WeatherImg.png is the same"
+            }
         }
+        
+
     }
     elseif ($jsonData.StatusCode -eq 401 -or $jsonData.StatusCode -eq 403) {
         Write-Host "[!] Issue with API key - weatherapi.com"
